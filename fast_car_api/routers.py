70ea133bb27eab91b224033b6e8921ba255ec8f9
Manoel <candidomanoel78@gmail.com>
2026-02-10 16:40:25 -0300

    feat: implementa rota para criar carro

--- fast_car_api/routers.py
+++ fast_car_api/routers.py
@@ -1,4 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, status
+from sqlalchemy.orm import Session
+from fast_car_api.database import get_session
+from fast_car_api.models import Car
+from fast_car_api.schemas import CarPublic, CarSchema
 
 router = APIRouter(
     prefix='/api/v1/cars',
@@ -6,3 +10,10 @@ router = APIRouter(
 )
 
 
+@router.post('/', response_model=CarPublic, status_code=status.HTTP_201_CREATED)
+def create_car(car: CarSchema, session: Session = Depends(get_session)):
+    car = Car(**car.model_dump())
+    session.add(car)
+    session.commit()
+    session.refresh(car)
+    return car
